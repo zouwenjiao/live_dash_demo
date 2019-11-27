ba5203e9c8871456b13d444f45b8140da41fe59a
zouwenjiao <45547666+zouwenjiao@users.noreply.github.com>
2019-11-26 16:47:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import vega_datasets
 app = dash.Dash(__name__, assets_folder='assets')
 #app.config['suppress_callback_exceptions'] = True
 server = app.server
-
-app = dash.Dash(__name__, assets_folder='assets')
 app.title = 'Dash app with pure Altair HTML'
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
